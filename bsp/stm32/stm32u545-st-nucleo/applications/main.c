71a6c1820f9596ff503ac0ab6313ddc2387f6e58
chenchi <chenchi_wy@163.com>
2024-07-26 16:55:26 +0800

    将 falsh 和 ram 的分区分别划分为了 bootloader 和 app 两部分

--- bsp/stm32/stm32u545-st-nucleo/applications/main.c
+++ bsp/stm32/stm32u545-st-nucleo/applications/main.c
@@ -20,11 +20,41 @@ void led_toggle(void) {
     rt_pin_write(LED2_PIN, !rt_pin_read(LED2_PIN));
 }
 
+extern char __bootloader_rom_start[];
+extern char __bootloader_rom_end[];
+extern char __bootloader_rom_occupied_end[];
+extern char __app_rom_start[];
+extern char __app_rom_end[];
+
+extern char __bootloader_ram_start[];
+extern char __bootloader_ram_end[];
+extern char __bootloader_ram_occupied_end[];
+extern char __app_ram_start[];
+extern char __app_ram_end[];
+
+extern char __heap_start[];
+extern char __heap_end[];
+
 int main(void)
 {
     int count = 1;
     /* set LED0 pin mode to output */
     rt_pin_mode(LED2_PIN, PIN_MODE_OUTPUT);
+
+    LOG_E("__bootloader_rom_start: %p\r\n", __bootloader_rom_start);
+    LOG_E("__bootloader_rom_end: %p\r\n", __bootloader_rom_end);
+    LOG_E("__bootloader_rom_occupied_end: %p\r\n", __bootloader_rom_occupied_end);
+    LOG_E("__app_rom_start: %p\r\n", __app_rom_start);
+    LOG_E("__app_rom_end: %p\r\n", __app_rom_end);
+
+    LOG_E("__bootloader_ram_start: %p\r\n", __bootloader_ram_start);
+    LOG_E("__bootloader_ram_end: %p\r\n", __bootloader_ram_end);
+    LOG_E("__bootloader_ram_occupied_end: %p\r\n", __bootloader_ram_occupied_end);
+    LOG_E("__app_ram_start: %p\r\n", __app_ram_start);
+    LOG_E("__app_ram_end: %p\r\n", __app_ram_end);
+
+    LOG_E("__heap_start: %p\r\n", __heap_start);
+    LOG_E("__heap_end: %p\r\n", __heap_end);
 
     while (count++)
     {
