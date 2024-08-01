0918bbf76418bd67b24285b6f76098d81b46b428
chenchi <chenchi_wy@163.com>
2024-08-01 15:35:43 +0800

    编译脚本中增加单独的 merge 命令

--- bsp/stm32/encortec/compile.py
+++ bsp/stm32/encortec/compile.py
@@ -3,6 +3,8 @@ import os
 import subprocess
 from tools.fw_merge import merge_bin_files
 
+ENCORTEC_FW_NAME = "./encortec-fw-all.bin"
+
 def build_bootloader():
     print("------ Building Bootloader...")
     subprocess.run("cd ./bootloader && scons -j8 && cd ..", shell=True)
@@ -23,25 +25,32 @@ def clean_app():
     subprocess.run("cd ./application && scons -c && cd ..", shell=True)
     print("------ Done!")
 
+def merge_fw():
+    merge_bin_files('./bootloader/encortec-bootloader.bin', './application/build/encortec-application.bin', ENCORTEC_FW_NAME)
+
+def clean_merged_fw():
+    print(f"------ Cleaning {ENCORTEC_FW_NAME}...")
+    if os.path.exists(ENCORTEC_FW_NAME):
+        os.remove(ENCORTEC_FW_NAME)
+    print("------ Done!")
+
 def build_all():
     build_bootloader()
     build_app()
-    merge_bin_files('./bootloader/encortec-bootloader.bin', './application/build/encortec-application.bin', './encortec-fw-all.bin')
+    merge_fw()
 
 def clean_all():
     clean_bootloader()
     clean_app()
-    print("------ Cleaning encortec-fw-all.bin...")
-    if os.path.exists('./encortec-fw-all.bin'):
-        os.remove('./encortec-fw-all.bin')
-    print("------ Done!")
+    clean_merged_fw()
 
 def print_usage(with_error=False):
     print(("Invalid command. " if with_error else "") + "Please use one of the following commands:")
     print("1. python ./compile.py -a|--app [-c|--clean] # Compile App code")
     print("2. python ./compile.py -b|--bootloader [-c|--clean] # Compile Bootloader code")
     print("3. python ./compile.py [-A|--all] [-c|--clean] # Compile Bootloader and then App")
-    print("3. python ./compile.py -h|--help # Print this message")
+    print("4. python ./compile.py -m|--merge [-c|--clean] # Merge Bootloader and App firmware")
+    print("5. python ./compile.py -h|--help # Print this message")
 
 
 """
@@ -51,11 +60,13 @@ python .\compile.py -c|--clean
 python .\compile.py -A|--all
 python .\compile.py -a|--app
 python .\compile.py -b|--bootloader
+python .\compile.py -m|--merge
 python .\compile.py -h|--help
 
 python .\compile.py -A|--all -c|--clean
 python .\compile.py -a|--app -c|--clean
 python .\compile.py -b|--bootloader -c|--clean
+python .\compile.py -m|--merge -c|--clean
 """
 if __name__ == "__main__":
     if len(sys.argv) == 1:
@@ -69,6 +80,8 @@ if __name__ == "__main__":
             build_app()
         elif sys.argv[1] in ['-b', '--bootloader']:
             build_bootloader()
+        elif sys.argv[1] in ['-m', '--merge']:
+            merge_fw()
         elif sys.argv[1] in ['-h', '--help']:
             print_usage()
         else:
@@ -83,6 +96,8 @@ if __name__ == "__main__":
                 clean_app()
             elif sys.argv[1] in ['-b', '--bootloader']:
                 clean_bootloader()
+            elif sys.argv[1] in ['-m', '--merge']:
+                clean_merged_fw()
             else:
                 print_usage(True)
     else:
