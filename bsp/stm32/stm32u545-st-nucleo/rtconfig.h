c3a366b4475f7743da72793b1209a1df50be1ddd
JhosuaSparrow <790041973@qq.com>
2024-08-05 16:24:05 +0800

    update: stm32 u545 u535 switch.

--- bsp/stm32/stm32u545-st-nucleo/rtconfig.h
+++ bsp/stm32/stm32u545-st-nucleo/rtconfig.h
@@ -1,11 +1,11 @@
 #ifndef RT_CONFIG_H__
 #define RT_CONFIG_H__
 
-// #define SOC_STM32U545RE
-// #define BOARD_STM32U545_NUCLEO
+#define SOC_STM32U545RE
+#define BOARD_STM32U545_NUCLEO
 
-#define SOC_STM32U535RE
-#define BOARD_STM32U535_NUCLEO
+// #define SOC_STM32U535RE
+// #define BOARD_STM32U535_NUCLEO
 
 /* RT-Thread Kernel */
 
