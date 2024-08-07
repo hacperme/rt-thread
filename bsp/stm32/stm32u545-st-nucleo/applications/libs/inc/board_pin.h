ac2a52ee75c1c20b11b61f89d2aae0875c0ab2b5
JhosuaSparrow <790041973@qq.com>
2024-08-07 11:43:57 +0800

    update: board_pin.h

--- bsp/stm32/stm32u545-st-nucleo/applications/libs/inc/board_pin.h
+++ bsp/stm32/stm32u545-st-nucleo/applications/libs/inc/board_pin.h
@@ -22,6 +22,9 @@ void nbiot_pwron_pin_init(void);
 void esp32_pwron_pin_init(void);
 void esp32_en_pin_init(void);
 void cat1_pwron_pin_init(void);
+void sim_select_pin_init(void);
+void nb_cat1_rf_pin_init(void);
+void intn_ext_ant_pin_init(void);
 
 rt_err_t gnss_pwron_pin_enable(rt_uint8_t mode);
 rt_err_t gnss_rst_pin_enable(rt_uint8_t mode);
@@ -32,6 +35,9 @@ rt_err_t nbiot_pwron_pin_enable(rt_uint8_t mode);
 rt_err_t esp32_pwron_pin_enable(rt_uint8_t mode);
 rt_err_t esp32_en_pin_enable(rt_uint8_t mode);
 rt_err_t cat1_pwron_pin_enable(rt_uint8_t mode);
+rt_err_t sim_select_pin_enable(rt_uint8_t mode);
+rt_err_t nb_cat1_rf_pin_enable(rt_uint8_t mode);
+rt_err_t intn_ext_ant_pin_enable(rt_uint8_t mode);
 
 int board_pins_init(void);
 
