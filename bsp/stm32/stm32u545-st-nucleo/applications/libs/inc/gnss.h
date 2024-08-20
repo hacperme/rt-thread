dfa549d11e98c81ab57136ad4531c009c26c8e01
JhosuaSparrow <790041973@qq.com>
2024-08-20 13:58:06 +0800

    update: gnss nmea parse.

--- bsp/stm32/stm32u545-st-nucleo/applications/libs/inc/gnss.h
+++ bsp/stm32/stm32u545-st-nucleo/applications/libs/inc/gnss.h
@@ -18,6 +18,22 @@
 #define GNSS_UART_NAME "uart4"
 #define GNSS_BUFF_SIZE 0x800
 
+typedef struct
+{
+    char GNRMC[100];
+    rt_uint8_t is_valid;
+    char GNGGA[100];
+    char GNGLL[100];
+    char GNVTG[100];
+    rt_uint8_t GNGSA_SIZE;
+    char GNGSA[5][100];
+    rt_uint8_t GPGSV_SIZE;
+    char GPGSV[5][100];
+} nmea_item;
+typedef nmea_item *nmea_item_t;
+
+void gnss_parse_nmea_item(char *item);
+void gnss_parse_nmea(char *nmea);
 static void gnss_thread_entry(void *parameter);
 static rt_err_t gnss_power_on(void);
 static rt_err_t gnss_power_off(void);
@@ -28,7 +44,9 @@ rt_err_t gnss_open(void);
 rt_err_t gnss_close(void);
 rt_err_t gnss_read_nmea(char *data, rt_uint32_t size);
 rt_err_t gnss_read_data(lwgps_t *gnss_data);
+rt_err_t gnss_read_nmea_item(nmea_item_t nmea_item);
 
+static rt_err_t test_show_nmea_item(nmea_item_t test_nmea_item);
 static void gnss_data_show(int argc, char **argv);
 
 #endif  // __GNSS_H__
--- bsp/stm32/stm32u545-st-nucleo/applications/libs/src/gnss.c
+++ bsp/stm32/stm32u545-st-nucleo/applications/libs/src/gnss.c
@@ -8,6 +8,7 @@
  */
 #include "gnss.h"
 #include <stdio.h>
+#include <string.h>
 
 #define DBG_SECTION_NAME "GNSS"
 #define DBG_LEVEL DBG_LOG
@@ -20,6 +21,136 @@ static lwgps_t hgps;
 static char nmea[GNSS_BUFF_SIZE];
 static struct rt_thread gnss_read_thd;
 static char gnss_read_thd_stack[0x1000];
+
+static nmea_item NMEA_ITEMS = {0};
+static const char NMEA_VALID_CHAR[4] = ",A,";
+static const char NMEA_START_CHAR[3] = "$G";
+static const char CRLF[3] = "\r\n";
+static const char NMEA_HEADRES[6][8] = {"$GNRMC", "$GNGGA", "$GNGLL", "$GNVTG", "$GNGSA", "$GPGSV"};
+
+void gnss_parse_nmea_item(char *item)
+{
+    // LOG_D("gnss_parse_nmea_item %s", item);
+    char *result;
+    char *first_comma;
+    result = rt_strstr(&item[2], NMEA_START_CHAR);
+    if (result)
+    {
+        gnss_parse_nmea_item(&item[result - item]);
+        return;
+    }
+    for (rt_uint8_t i = 0; i < 6; i++)
+    {
+        result = rt_strstr(item, NMEA_HEADRES[i]);
+        if (result)
+        {
+            if (i == 0)
+            {
+                rt_strncpy(NMEA_ITEMS.GNRMC, item, rt_strlen(item));
+                result = rt_strstr(&NMEA_ITEMS.GNRMC[7], NMEA_VALID_CHAR);
+                if (result)
+                {
+                    first_comma = strchr(&NMEA_ITEMS.GNRMC[7], ',');
+                    NMEA_ITEMS.is_valid = (first_comma != NULL && result == first_comma) ? 1: 0;
+                }
+                else
+                {
+                    NMEA_ITEMS.is_valid = 0;
+                }
+            }
+            else if (i == 1)
+            {
+                rt_strncpy(NMEA_ITEMS.GNGGA, item, rt_strlen(item));
+            }
+            else if (i == 2)
+            {
+                rt_strncpy(NMEA_ITEMS.GNGLL, item, rt_strlen(item));
+            }
+            else if (i == 3)
+            {
+                rt_strncpy(NMEA_ITEMS.GNVTG, item, rt_strlen(item));
+            }
+            else if (i == 4)
+            {
+                if (NMEA_ITEMS.GNGSA_SIZE < 5)
+                {
+                    rt_strncpy(NMEA_ITEMS.GNGSA[NMEA_ITEMS.GNGSA_SIZE], item, rt_strlen(item));
+                    NMEA_ITEMS.GNGSA_SIZE++;
+                }
+            }
+            else if (i == 5)
+            {
+                if (NMEA_ITEMS.GPGSV_SIZE < 5)
+                {
+                    rt_strncpy(NMEA_ITEMS.GPGSV[NMEA_ITEMS.GPGSV_SIZE], item, rt_strlen(item));
+                    NMEA_ITEMS.GPGSV_SIZE++;
+                }
+            }
+            LOG_D("NMEA: %s", item);
+            return;
+        }
+    }
+}
+
+#if 0
+void gnss_parse_nmea_bak(char *nmea)
+{
+    char *result;
+    char nmea_item[100];
+    rt_uint32_t start_index, end_index;
+    start_index = end_index = 0;
+
+    while (1)
+    {
+        result = rt_strstr(&nmea[end_index], NMEA_START_CHAR);
+        if (result)
+        {
+            start_index = result - nmea;
+            // LOG_D("start_index=%d", start_index);
+            result = rt_strstr(&nmea[start_index], CRLF);
+            if (result)
+            {
+                end_index = result - nmea;
+                rt_memset(nmea_item, '\0', 100);
+                rt_strncpy(nmea_item, &nmea[start_index], (end_index - start_index));
+                // LOG_D("end_index=%d", end_index);
+                gnss_parse_nmea_item(nmea_item);
+                end_index += 2;
+                if (end_index >= rt_strlen(nmea))
+                {
+                    break;
+                }
+            }
+            else
+            {
+                // LOG_I("Can not find CRLF in nmea");
+                break;
+            }
+        }
+        else
+        {
+            // LOG_I("Can not find %s in nmea", NMEA_START_CHAR);
+            break;
+        }
+    }
+}
+#endif
+
+void gnss_parse_nmea(char *nmea)
+{
+    char *nmea_item;
+
+    rt_memset(&NMEA_ITEMS, 0, sizeof(NMEA_ITEMS));
+
+    // This function will replace CRLF to \0\0, so if need to show nmea, should pay attention to that.
+    nmea_item = strtok(nmea, CRLF);
+    while (nmea_item != NULL)
+    {
+        gnss_parse_nmea_item(nmea_item);
+        nmea_item = strtok(NULL, CRLF);
+    }
+
+}
 
 static rt_err_t gnss_power_on(void)
 {
@@ -64,8 +195,9 @@ static void gnss_thread_entry(void *parameter)
                 // LOG_D("===================================");
                 // LOG_D("\r\n%s", nmea);
                 // LOG_D("===================================");
-                LOG_D("NMEA Size %d", rt_strlen(nmea));
+                // LOG_D("NMEA Size %d", rt_strlen(nmea));
                 lwgps_process(&hgps, nmea, rt_strlen(nmea));
+                gnss_parse_nmea(nmea);
             }
         }
         else
@@ -144,6 +276,10 @@ rt_err_t gnss_close(void)
     return gnss_power_off();
 }
 
+/*
+If not necessary, not to use this function to read source nmea.
+Because CRLF in nmea is all replaced to \0\0. 
+*/
 rt_err_t gnss_read_nmea(char *data, rt_uint32_t size) {
     rt_err_t res = RT_ERROR;
     rt_err_t result;
@@ -183,6 +319,49 @@ rt_err_t gnss_read_data(lwgps_t *gnss_data)
     return res;
 }
 
+rt_err_t gnss_read_nmea_item(nmea_item_t nmea_item)
+{
+    rt_err_t res = RT_ERROR;
+    rt_err_t result;
+    result = rt_mutex_take(GNSS_LOCK, RT_WAITING_FOREVER);
+    if (result == RT_EOK)
+    {
+        rt_memset(nmea_item, 0, sizeof(*nmea_item));
+        rt_memcpy(nmea_item, &NMEA_ITEMS, sizeof(NMEA_ITEMS));
+        res = RT_EOK;
+    }
+    else
+    {
+        LOG_E("Get GNSS_LOCK to read gnss data failed.");
+    }
+    rt_mutex_release(GNSS_LOCK);
+    return res;
+}
+
+static rt_err_t test_show_nmea_item(nmea_item_t test_nmea_item)
+{
+    rt_err_t res = RT_ERROR;
+    rt_err_t result;
+    rt_uint8_t i;
+    gnss_read_nmea_item(test_nmea_item);
+    LOG_D("test_nmea_item->GNRMC: %s", test_nmea_item->GNRMC);
+    LOG_D("test_nmea_item->is_valid: %d", test_nmea_item->is_valid);
+    LOG_D("test_nmea_item->GNGGA: %s", test_nmea_item->GNGGA);
+    LOG_D("test_nmea_item->GNGLL: %s", test_nmea_item->GNGLL);
+    LOG_D("test_nmea_item->GNVTG: %s", test_nmea_item->GNVTG);
+    LOG_D("test_nmea_item->GNGSA_SIZE: %d", test_nmea_item->GNGSA_SIZE);
+    for (i = 0; i < test_nmea_item->GNGSA_SIZE; i++)
+    {
+        LOG_D("test_nmea_item->GNGSA[%d]: %s", i, test_nmea_item->GNGSA[i]);
+    }
+    LOG_D("test_nmea_item->GPGSV_SIZE: %d", test_nmea_item->GPGSV_SIZE);
+    for (i = 0; i < test_nmea_item->GPGSV_SIZE; i++)
+    {
+        LOG_D("test_nmea_item->GPGSV[%d]: %s", i, test_nmea_item->GPGSV[i]);
+    }
+    return res;
+}
+
 static void gnss_data_show(int argc, char **argv)
 {
     gnss_open();
@@ -190,8 +369,11 @@ static void gnss_data_show(int argc, char **argv)
     char gnss_data[2048] = {0};
     char msg[256];
     rt_uint8_t cnt = 0;
+    nmea_item test_nmea_item = {0};
+
     while (cnt < 60)
     {
+        /* Test to show GNSS data */
         sprintf(msg, "GNSS Date: %d-%d-%d %d:%d:%d", hgps.year, hgps.month, hgps.date, 
                 hgps.hours, hgps.minutes, hgps.seconds);
         LOG_D(msg);
@@ -203,11 +385,14 @@ static void gnss_data_show(int argc, char **argv)
         LOG_D(msg);
         sprintf(msg, "Altitude: %f meters", (float)hgps.altitude);
         LOG_D(msg);
-        rt_memset(gnss_data, 0, 2048);
-        gnss_read_nmea(gnss_data, 2048);
-        LOG_D("=============================");
-        LOG_D("\r\n%s", gnss_data);
-        LOG_D("=============================");
+
+        /* Test to show NMEA item data */
+        test_show_nmea_item(&test_nmea_item);
+        // rt_memset(gnss_data, 0, 2048);
+        // gnss_read_nmea(gnss_data, 2048);
+        // LOG_D("=============================");
+        // LOG_D("\r\n%s", gnss_data);
+        // LOG_D("=============================");
         cnt++;
         rt_thread_mdelay(1000); //at least 300 ms
     }
