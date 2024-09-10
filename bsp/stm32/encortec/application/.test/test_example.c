790c6c6bd376cca2e3bc065830a673f6fa411df9
JhosuaSparrow <790041973@qq.com>
2024-09-10 21:24:50 +0800

    update: test code

--- bsp/stm32/encortec/application/.test/test_example.c
+++ bsp/stm32/encortec/application/.test/test_example.c
@@ -18,8 +18,8 @@ int application_start(int argc, char *argv[]) {
     // extern rt_err_t test_fdc1004(int argc, char **argv);
     // test_fdc1004(argc, argv);
 
-    extern void test_gnss(int argc, char **argv);
-    test_gnss(argc, argv);
+    // extern void test_gnss(int argc, char **argv);
+    // test_gnss(argc, argv);
 
     // extern rt_err_t test_hdc3021(void);
     // test_hdc3021();
